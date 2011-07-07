ff22e8a55416403a7fc45ec6553e6df0fbc238e2
Matteo Beccati <matteo@beccati.com>
2011-07-07 16:05:09 +0200

    Removed old code

--- app/code/community/Fooman/Jirafe/Block/Js.php
+++ app/code/community/Fooman/Jirafe/Block/Js.php
@@ -27,147 +27,15 @@ class Fooman_Jirafe_Block_Js extends Mage_Core_Block_Template
      * Set default template
      *
      */
-    protected function _construct ()
+    protected function _construct()
     {
         $this->setTemplate('fooman/jirafe/js.phtml');
     }
 
-    protected function _getSession ()
+    protected function _getSession()
     {
         return Mage::getSingleton('customer/session');
     }
-
-//    protected $_isCheckoutSuccess = false;
-// 
-//    public function setIsCheckoutSuccess ($flag)
-//    {
-//        $this->_isCheckoutSuccess = $flag;
-//    }
-//
-//    public function getIsCheckoutSuccess ()
-//    {
-//        return $this->_isCheckoutSuccess;
-//    }
-//
-//    public function setPiwikVisitorType ($type=null)
-//    {
-//        $currentType = $this->_getSession()->getPiwikVisitorType();
-//        if (empty($currentType)) {
-//            $this->_getSession()->setPiwikVisitorType(self::VISITOR_ALL);
-//            Mage::register('piwik_visitor_type_set', true);
-//        } elseif ($type > $currentType) {
-//            $this->_getSession()->setPiwikVisitorType($type);
-//        } elseif ($currentType == self::VISITOR_ALL && $type == self::VISITOR_ALL) {
-//            //upgrade to browser on second page view
-//            if (!Mage::registry('piwik_visitor_type_set')) {
-//                $this->_getSession()->setPiwikVisitorType(self::VISITOR_BROWSERS);
-//            }
-//        }
-//    }
-//
-//    public function getPiwikVisitorType ()
-//    {
-//        $currentType = $this->_getSession()->getPiwikVisitorType();
-//        $this->setPiwikVisitorType(self::VISITOR_ALL);
-//        return $this->_getSession()->getPiwikVisitorType();
-//    }
-//
-//    public function getTrackingInfo ()
-//    {
-//        $js = "";
-//        $js .= $this->_getPageTrackingInfo() . "\n";
-//        //$js .= $this->_getCartTrackingInfo()."\n";
-//        $js .= $this->_getPurchaseTrackingInfo() . "\n";
-//        Mage::helper('foomanjirafe')->debug($js);
-//        return $js;
-//    }
-//
-//    
-//    public function getAdditionalTrackingInfo ()
-//    {
-//        return "";
-//    }
-//
-//    public function _getPageTrackingInfo ()
-//    {
-//        $js = "";
-//        if ($this->getIsCheckoutSuccess()) {
-//            //$js .= "_paq.push(['setCustomVariable',5, 'orderId','".$this->_getLastOrderIncrementId()."']);";
-//        }
-//        $js .= "_paq.push(['setCustomVariable','1','U','" . $this->getPiwikVisitorType() . "']);
-//        _paq.push(['trackPageView']);";
-//        return $js;
-//    }
-//
-//    public function _getCartTrackingInfo ()
-//    {
-//        return "";
-//    }
-//
-//    public function _getPurchaseTrackingInfo ()
-//    {
-//        $js = "";
-//        if ($this->getIsCheckoutSuccess()) {
-//            $items = array();
-//            $quote = $this->_getLastQuote();
-//            if ($quote) {
-//                foreach ($quote->getAllVisibleItems() as $quoteItem) {
-//                    $items[] = array('sku' => $quoteItem->getSku(), 'price' => $quoteItem->getBasePrice());
-//                }
-//                //$js .= "_paq.push(['trackGoal',".Mage::helper('foomanjirafe')->getStoreConfig('checkoutGoalId', Mage::app()->getStore()->getId()).",'".$quote->getBaseGrandTotal()."']);";
-//            }
-//        }
-//        return $js;
-//    }
-//    
-//    /**
-//     * load the quote belonging to the last successful order
-//     * 
-//     * @return Mage_Sales_Model_Quote|bool
-//     */
-//    public function _getLastQuote ()
-//    {
-//        $orderIncrementId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
-//        if ($orderIncrementId) {
-//            $order = Mage::getModel('sales/order')->loadByIncrementId($orderIncrementId);
-//            if ($order->getId()) {
-//                $quoteId = $order->getQuoteId();
-//            }
-//        } else {
-//            $quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
-//        }
-//
-//        if ($quoteId) {
-//            $quote = Mage::getModel('sales/quote')->load($quoteId);
-//            if ($quote->getId()) {
-//                return $quote;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    /**
-//     * load the quote belonging to the last successful order
-//     *
-//     * @return string|bool
-//     */
-//    public function _getLastOrderIncrementId ()
-//    {
-//        $orderIncrementId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
-//        if ($orderIncrementId) {
-//            return $orderIncrementId;
-//        } else {
-//            $quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
-//        }
-//
-//        if ($quoteId) {
-//            $quote = Mage::getModel('sales/quote')->load($quoteId);
-//            if ($quote->getId()) {
-//                return $quote->getReservedOrderId();
-//            }
-//        }
-//        return false;
-//    }
 
     public function getSiteId()
     {
