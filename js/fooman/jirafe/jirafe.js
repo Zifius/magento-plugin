c885f71a83a40237d492b1967f893ff1be0f395d
Kristof Ringleff <kristof@fooman.co.nz>
2011-08-25 10:52:58 +1200

    change timing of js unreachable notice

--- js/fooman/jirafe/jirafe.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function() {
-    if ($('mod-jirafe') == undefined){
-        $('messages').insert ("<ul class=\"messages\"><li class=\"error-msg\">We're unable to connect with the Jirafe service for the moment. Please wait a few minutes and refresh this page later.</li></ul>");        
-    }        
-}, 1500);
