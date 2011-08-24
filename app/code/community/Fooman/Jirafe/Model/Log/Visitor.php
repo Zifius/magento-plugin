1100c68a0932085f7069e79b227bbf11fb70efb5
Kristof Ringleff <kristof@fooman.co.nz>
2011-08-24 16:41:28 +1200

    extend support to lower 1.3 versions - removes additional useragent filters

--- app/code/community/Fooman/Jirafe/Model/Log/Visitor.php
+++ app/code/community/Fooman/Jirafe/Model/Log/Visitor.php
@@ -21,26 +21,28 @@ class Fooman_Jirafe_Model_Log_Visitor extends Mage_Log_Model_Visitor
     protected function _construct()
     {
         parent::_construct();
-        $userAgent = Mage::helper('core/http')->getHttpUserAgent();
-        if (!$this->_skipRequestLogging) {
-            if (empty ($userAgent)){
-                $this->_skipRequestLogging = true;
-            }
-        }
-
-        //ignore user agents was introduced in 1.4.0.0
-        if(!$this->_skipRequestLogging && version_compare(Mage::getVersion(), '1.4.0.0')  < 0) {
-            $ignoreAgents = Mage::getConfig()->getNode('global/ignore_user_agents');
-            if ($ignoreAgents) {
-                $ignoreAgents = $ignoreAgents->asArray();
-                if (in_array($userAgent, $ignoreAgents)) {
+        if(version_compare(Mage::getVersion(), '1.3.2.2', '>')) {
+            $userAgent = Mage::helper('core/http')->getHttpUserAgent();
+            if (!$this->_skipRequestLogging) {
+                if (empty ($userAgent)){
                     $this->_skipRequestLogging = true;
                 }
             }
-        }
-        if (!$this->_skipRequestLogging) {
-            if(preg_match("/".self::USER_AGENT_BOT_PATTERN."/i", $userAgent)) {
-                $this->_skipRequestLogging = true;
+
+            //ignore user agents was introduced in 1.4.0.0
+            if(!$this->_skipRequestLogging && version_compare(Mage::getVersion(), '1.4.0.0')  < 0) {
+                $ignoreAgents = Mage::getConfig()->getNode('global/ignore_user_agents');
+                if ($ignoreAgents) {
+                    $ignoreAgents = $ignoreAgents->asArray();
+                    if (in_array($userAgent, $ignoreAgents)) {
+                        $this->_skipRequestLogging = true;
+                    }
+                }
+            }
+            if (!$this->_skipRequestLogging) {
+                if(preg_match("/".self::USER_AGENT_BOT_PATTERN."/i", $userAgent)) {
+                    $this->_skipRequestLogging = true;
+                }
             }
         }
     }
