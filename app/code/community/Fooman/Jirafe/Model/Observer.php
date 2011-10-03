ec74bfe42443c08da01ded88af48d370e9449c8d
Kristof Ringleff <kristof@fooman.co.nz>
2011-10-04 09:45:59 +1300

    use the base product sku - works with bundles

--- app/code/community/Fooman/Jirafe/Model/Observer.php
+++ app/code/community/Fooman/Jirafe/Model/Observer.php
@@ -477,7 +477,7 @@ class Fooman_Jirafe_Model_Observer
                     $itemPrice = $item->getPrice();
                 }
                 $piwikTracker->addEcommerceItem(
-                    $item->getProduct()->getSku(),
+                    $item->getProduct()->getData('sku'),
                     $item->getName(),
                     $this->_getCategory($item->getProduct()),
                     $itemPrice,
