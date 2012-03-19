eb711e7a62942deeb71c5f027b51edeab5a3802d
Kristof Ringleff <kristof@fooman.co.nz>
2012-03-20 01:06:15 +1300

    remove merge debris

--- app/code/community/Fooman/Jirafe/Model/Mysql4/Event/Collection.php
+++ app/code/community/Fooman/Jirafe/Model/Mysql4/Event/Collection.php
@@ -13,13 +13,6 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-<<<<<<< HEAD
-// Modify tables with the new DB schema
-Mage::helper('foomanjirafe/setup')->runDbSchemaUpgrade($installer, $version);
-
-//Run sync when finished with install/update
-Mage::getModel('foomanjirafe/jirafe')->initialSync($version);
-=======
 class Fooman_Jirafe_Model_Mysql4_Event_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
     protected function _construct()
@@ -27,4 +20,3 @@ class Fooman_Jirafe_Model_Mysql4_Event_Collection extends Mage_Core_Model_Mysql4
         $this->_init('foomanjirafe/event');
     }
 }
->>>>>>> parent of 2c01def... prepare 0.4.0 release with enable question
