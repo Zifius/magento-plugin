601fe88dabd9c055dfbc7b6e0f6d069ac7a88394
Kristof Ringleff <kristof@fooman.co.nz>
2012-07-13 14:17:45 +0200

    display versions under status

--- app/code/community/Fooman/Jirafe/Block/Adminhtml/Status.php
+++ app/code/community/Fooman/Jirafe/Block/Adminhtml/Status.php
@@ -66,7 +66,13 @@ class Fooman_Jirafe_Block_Adminhtml_Status extends Mage_Adminhtml_Block_Template
         $conn = Mage::getSingleton('core/resource');
         $read = $conn->getConnection('core_read');
         return $read->fetchAll("SELECT * FROM `{$conn->getTableName('core_config_data')}` WHERE path like 'foomanjirafe%'");
+    }
 
+    public function getJirafeInstalledVersion()
+    {
+        $conn = Mage::getSingleton('core/resource');
+        $read = $conn->getConnection('core_read');
+        return $read->fetchOne("SELECT version FROM `{$conn->getTableName('core_resource')}` WHERE code like 'foomanjirafe_setup'");
     }
 
     public function getOrderSyncStatus()
